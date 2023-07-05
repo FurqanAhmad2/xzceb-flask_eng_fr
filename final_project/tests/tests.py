5bba963bbeba77b1db3c01898dc5aa5b6f03ceef
FurqanAhmad2 <furqana405@gmail.com>
2023-07-05 11:44:10 +0500

    Final Chnages Now

--- final_project/tests/tests.py
+++ final_project/tests/tests.py
@@ -1,13 +1,19 @@
 import unittest
-from machinetranslation.translator import english_to_french, french_to_english
+from translator import english_to_french, french_to_english
 
 class TestEngToFrench(unittest.TestCase):
-    def test1(self):
+    def test_equal_translation(self):
         self.assertEqual(english_to_french("Hello"), "Pepitoooo")
 
+    def test_not_equal_translation(self):
+        self.assertNotEqual(english_to_french("Hello"), "Salut")
+
 class TestFrenchToEnglish(unittest.TestCase):
-    def test1(self):
-        self.assertEqual(french_to_english("Pepitoooo"), "Hello" )
+    def test_equal_translation(self):
+        self.assertEqual(french_to_english("Pepitoooo"), "Hello")
+
+    def test_not_equal_translation(self):
+        self.assertNotEqual(french_to_english("Bonjour"), "Salut")
 
 if __name__ == "__main__":
     unittest.main()
